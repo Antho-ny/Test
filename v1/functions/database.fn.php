9d8a0b818d293c20d8dd96970a1a7ae2b6bac20b
Antho-ny <Antho@MacBook-Pro-de-Antho-ny.local>
2013-09-27 14:48:59 +0200

    DEl V1

--- v1/functions/database.fn.php
+++
@@ -1,16 +0,0 @@
-<?php
-// version 1
-// editeur : gaspard
-/**
- * fonction de connection de base de donnee
- * @param $db (array) > mes identifiants de base 
- * @return lien de base
- */function database_connect($db){	$link = mysql_connect($db['host'],$db['user'],$db['pass']);	if(!$link) die("erreur de connexion a la base de donnee".mysql_error());	if(!mysql_select_db($db['base'])) die ("selection de la vase impossible");	return $link;}
-
-/**
- * fonction de deconnection de base de donnee
- * @param $link lien de base
- * @return rien
- */
-function database_disconnect($link){	mysql_close($link);
-}
